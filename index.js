2181791aba926fa46d5c8e5dfe357199f4328dad
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-20 21:38:14 -0400

    commit tree dcc0d915dae4a1d80f17c0b74b224b3d56bb06c8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(currentBlock) {
+    return Math.abs(currentBlock - 42);
+}
+
+function distanceFromHqInFeet(currentBlock) {
+    return 264 * distanceFromHqInBlocks(currentBlock);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return 264 * Math.abs(start - destination);
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) return 'cannot travel that far';
+    else if (distance <= 400) return 0;
+    else if (distance <= 2000) return (distance - 400) * 0.02;
+    else return 25;
+}
